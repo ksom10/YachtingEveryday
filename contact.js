c469270b2a6cb909e8d1d2e722866a61ca2a6799
ksom10 <someson11@aol.com>
2024-02-28 16:44:08 -0500

    spinner

--- contact.js
+++ contact.js
@@ -1,15 +1,9 @@
-const form = document.querySelector('form');
+document.getElementById('contact').addEventListener('submit', function() {
+    // Show the spinner when the form is submitted
+    document.getElementById('loading-spinner').style.display = 'block';
+});
 
-function sendEmail(){
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "yachtingeveryday@gmail.com",
-        Password : "21FD34E5275EE5658B66B502CF3884CB5C13",
-        To : 'them@website.com',
-        From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
-}
+window.onload = function() {
+    // Hide the spinner when the next page loads
+    document.getElementById('loading-spinner').style.display = 'none';
+};
